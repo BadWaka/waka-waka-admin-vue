6cd79234c825a7bafaa111d8f1d04052920ea986
BadWaka <1456683844@qq.com>
2017-09-07 15:55:30 +0800

    加入sass

--- src/containers/Index/Index.vue
+++ src/containers/Index/Index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="index">
     Index
+    <div class="a">
+      <div class="b"></div>
+    </div>
   </div>
 </template>
 
@@ -15,23 +18,12 @@
   }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-  h1, h2 {
-    font-weight: normal;
-  }
+<style scoped lang="scss">
+  .a {
 
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-
-  a {
-    color: #42b983;
+    .b {
+      height: 20px;
+      background: red;
+    }
   }
 </style>
