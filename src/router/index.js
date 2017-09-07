997449c26c91dc8aee6676aa4249d6579c7284df
BadWaka <1456683844@qq.com>
2017-09-07 15:44:51 +0800

    主页

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '../components/Hello.vue';
+import Index from '../containers/Index/Index.vue';
 import AddOrEditArticle from '../containers/AddOrEditArticle/AddOrEditArticle.vue';
 
 Vue.use(Router);
@@ -9,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Index',
+      component: Index
     }
   ]
 })
